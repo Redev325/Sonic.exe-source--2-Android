b0b8fd9249ba262670d58397fa922f9db4cadb35
Redev325 <s1991375@online.houstonisd.org>
2026-09-18 21:33:45 -0500

    Initialize Sonic.EXE Freeplay saves on HTML5

--- source/KadeEngineData.hx
+++ source/KadeEngineData.hx
@@ -6,7 +6,8 @@ class KadeEngineData
 {
     public static function initSave()
     {
-		if (FlxG.save.data.songArray == null) FlxG.save.data.songArray = [];
+		if (FlxG.save.data.songArray == null || FlxG.save.data.songArray.length == 0)
+			FlxG.save.data.songArray = ["endless", "cycles", "milk", "sunshine", "faker", "black-sun", "chaos"];
 		
 		if (FlxG.save.data.storyProgress == null) // Data erase fuck you.
 		{
