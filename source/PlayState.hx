967d7f43421ce55fd2fa1064fdda487d87eef96c
Redev325 <s1991375@online.houstonisd.org>
2026-09-19 12:59:08 -0500

    Add visible ring note hit scale animation

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -6398,10 +6398,27 @@ class PlayState extends MusicBeatState
 			note.wasGoodHit = true;
 			setVocalsVolumeSafe(1);
 
-			note.kill();
-			notes.remove(note, true);
-
-			note.destroy();
+			// Ring notes need a visible hit animation before they disappear.
+			if (isRing && note.noteData == 2 && !note.isSustainNote)
+			{
+				var ringHitScale = note.scale.x * 1.38;
+				FlxTween.tween(note.scale, {x: ringHitScale, y: ringHitScale}, 0.12, {
+					ease: FlxEase.quadOut,
+					onComplete: function(tween:FlxTween)
+					{
+						note.kill();
+						notes.remove(note, true);
+						note.destroy();
+					}
+				});
+				FlxTween.tween(note, {alpha: 0}, 0.12, {ease: FlxEase.quadOut});
+			}
+			else
+			{
+				note.kill();
+				notes.remove(note, true);
+				note.destroy();
+			}
 
 			updateAccuracy();
 		}
