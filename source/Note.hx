57b82495fcf6d3e73964658f46d0f4072e3c8a7e
Redev325 <s1991375@online.houstonisd.org>
2026-09-19 16:43:58 -0500

    Make Triple Trouble ring notes render reliably

--- source/Note.hx
+++ source/Note.hx
@@ -224,7 +224,7 @@ class Note extends FlxSprite
 								animation.addByPrefix('redScroll', 'red0');
 								animation.addByPrefix('blueScroll', 'blue0');
 								animation.addByPrefix('purpleScroll', 'purple0');
-								if (isRing) animation.addByPrefix('goldScroll', 'gold0');
+								if (isRing) animation.addByPrefix('goldScroll', 'gold');
  
 								animation.addByPrefix('purpleholdend', 'pruple end hold');
 								animation.addByPrefix('greenholdend', 'green hold end');
